99ba953eff6f689846970e3a22347bc3f5fc3b98
Crowdbotics <team@crowdbotics.com>
2020-10-08 04:24:00 +0000

    Update components

--- src/features/BlankScreen0132938/screens/blank.js
+++ src/features/BlankScreen0132938/screens/blank.js
@@ -1,23 +1,46 @@
-import React from "react";
-import { View, Image, ImageBackground, TouchableOpacity } from "react-native";
-import { withStyles, Text, Button, CheckBox, Radio, Toggle, Icon, Input, Datepicker } from 'react-native-ui-kitten';
-import Slider from '@react-native-community/slider';
+import React from "react"
+import { View, Image, ImageBackground } from "react-native"
+import {
+  withStyles,
+  Text,
+  Button,
+  CheckBox,
+  Radio,
+  Toggle,
+  Icon,
+  Input,
+  Datepicker
+} from "react-native-ui-kitten"
+import Slider from "@react-native-community/slider"
+
 import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export class _Blank extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
 
-  state = { }
+  state = { TextInput_7: "cxzcxz" }
 
-  render = () => <View style={this.props.themedStyle.container}></View>;
+  render = () => (
+    <View style={styles.View_1}>
+      <View style={styles.View_5}>
+        <TextInput
+          placeholder="Sample text input placeholder"
+          editable={true}
+          style={styles.TextInput_7}
+          value={this.state.TextInput_7}
+          onChangeText={nextValue => this.setState({ TextInput_7: nextValue })}
+        />
+      </View>
+    </View>
+  )
 }
 
 export default Blank = withStyles(_Blank, theme => ({
   container: {
     backgroundColor: theme["color-basic-100"]
   }
-}));
+}))
